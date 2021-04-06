956c6d54f219cd7f4f0cf944e6ef15b6f97da340
Oderystella <ddott59@gmail.com>
2021-04-06 11:37:48 +0900

    feat: fix code for image-upload

--- js/app.js
+++ js/app.js
@@ -1,21 +1,22 @@
-const $imageUploader = document.querySelector('.image-input'); // file input node
+const $imageInput = document.querySelector('.image-input'); // file input node
 const $imageView = document.querySelector('.image-view'); // image node
-const reader = new FileReader(); // 비동기적으로 유저 컴퓨터에 저장된 파일을 읽어옴
 
-/* localStorage에서 recentImage의 value값을 가져와 이미지 경로를 다시 지정해주는 함수 */
+
+/* localStorage에서 이미지 경로 읽어와서 보여주는 함수 */
+/* localStorage에서 recentImage의 value값을 가져와 이미지 경로를 다시 지정해줌 */
 const localStorageImage = () => {
-  const recentImageDataUrl = localStorage.getItem('recentImage'); // key가 recentImage인 value값
+  const recentImageDataUrl = localStorage.getItem('recentImage'); // key가 recentImage인 value값을 읽어옴
   if (recentImageDataUrl) $imageView.setAttribute('src', recentImageDataUrl)
 };
 
 
 /* 이벤트 핸들러 부분*/
-document.addEventListener('DOMContentLoaded', localStorageImage);
 
-reader.onload = () => localStorage.setItem('recentImage', reader.result); // recentImage value에 reader가 읽어온 result를 지정한다.
-
-$imageUploader.onchange = () => {
-  reader.readAsDataURL($imageUploader.files[0]); // readAsDataURL가 file에서 이미지를 읽어온 후 image-view에서 보여줌
+$imageInput.onchange = () => {
+  const reader = new FileReader(); // 비동기적으로 유저 컴퓨터에 저장된 파일을 읽어옴
+  reader.onload = () => localStorage.setItem('recentImage', reader.result); // recentImage value에 reader가 읽어온 result를 지정함. localStorage에 이미지 저장하는 함수 
+  reader.readAsDataURL($imageInput.files[0]); // readAsDataURL가 file에서 이미지를 읽어온 후 image-view에서 보여줌
   console.log('success');
 };
 
+document.addEventListener('DOMContentLoaded', localStorageImage);
