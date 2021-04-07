88cad8af22a9c4e18e9a50f1a6435c1f264b34e9
Oderystella <ddott59@gmail.com>
2021-04-07 18:49:46 +0900

    feat: add remove button for an image

--- js/remove.js
+++ js/remove.js
@@ -1,15 +1,38 @@
 
-const removeButton = document.querySelector('.delete-button');
+const removeButton1 = document.querySelector('.delete-button1');
+const removeButton2 = document.querySelector('.delete-button2');
+const removeButton3 = document.querySelector('.delete-button3');
+const removeButton4 = document.querySelector('.delete-button4');
+const removeButton5 = document.querySelector('.delete-button5');
+const removeButton6 = document.querySelector('.delete-button6');
 
-removeButton.onclick = () => {
-    console.log(localStorage);
-    localStorage.removeItem('recentImage1');
-    localStorage.removeItem('recentImage2');
-    localStorage.removeItem('recentImage3');
-    localStorage.removeItem('recentImage4');
-    localStorage.removeItem('recentImage5');
-    localStorage.removeItem('recentImage6');
+removeButton1.onclick = () => {
+  console.log('remove');
+  localStorage.removeItem('recentImage1');
 };
 
+removeButton2.onclick = () => {
+  console.log('remove');
+  localStorage.removeItem('recentImage2');
+};
 
+removeButton3.onclick = () => {
+  console.log('remove');
+  localStorage.removeItem('recentImage3');
+};
+
+removeButton4.onclick = () => {
+  console.log('remove');
+  localStorage.removeItem('recentImage4');
+};
+
+removeButton5.onclick = () => {
+  console.log('remove');
+  localStorage.removeItem('recentImage5');
+};
+
+removeButton6.onclick = () => {
+    console.log(remove);
+    localStorage.removeItem('recentImage6');
+};
 
